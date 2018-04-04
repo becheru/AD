6b696a330335e50dec3cb1a23afe2298a8cf2623
Alex Becheru <becheru@gmail.com>
2018-04-04 12:39:28 +0300

    Added Linked Lists, Queues & Stacks

---
+++ Linked Lists/Double Linked Lists/CodeSkeleton-CodeBlocks/main.c
@@ -0,0 +1,302 @@
+//
+// Created by Alexandru Becheru on 20/03/2018.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+struct g_node{
+    int info;
+    struct g_node *next;
+    struct g_node *previous;
+};
+
+void interchange_nodes_values(struct g_node *node_1, struct g_node *node_2){
+    int aux;
+
+    aux = node_1->info;
+    node_1->info = node_2->info;
+    node_2->info = aux;
+}
+
+void push_element_begining(struct g_node *head, struct g_node *tail ,int new_element_value){
+    struct g_node *new_element;
+    new_element = malloc(sizeof(struct g_node));
+    new_element->info = new_element_value;
+    struct g_node *next_element;
+
+    next_element = head->next;
+    if (next_element != NULL){
+        next_element->previous = new_element;
+    }
+    new_element->next = next_element;
+    new_element->previous = head;
+    head->next = new_element;
+    head->info++;
+}
+
+int get_no_elements(struct g_node *head, struct g_node *tail){
+    int no_elements ;
+    struct g_node *iterator_left;
+    struct g_node *iterator_right;
+
+    if (head == tail){
+        return 0;
+    }
+    iterator_left = head->next;
+    iterator_right = tail->previous;
+    no_elements = 0;
+    while ( 1 > 0 ){
+        if (iterator_left == iterator_right){
+            return no_elements + 1;
+        }
+        if (iterator_left->next == iterator_right){
+            return  no_elements + 2;
+        }
+        no_elements = no_elements + 2;
+        iterator_left = iterator_left->next;
+        iterator_right= iterator_right->previous;
+    }
+}
+
+///to be implemented
+void push_element_end(struct g_node *head ,struct g_node *tail ,int new_element_value){
+    struct g_node *new_element;
+    struct g_node *last_element;
+
+}
+
+void push_element_at_position(struct g_node *head, struct g_node *tail, int position, int new_element_value){
+    int no_elements;
+    int current_positon;
+    struct g_node *iterator;
+    struct g_node *previous_element;
+    struct g_node *next_element;
+    struct g_node *new_element;
+
+    no_elements = head->info;
+    assert(position >= 0);
+    assert(position <= no_elements);
+
+    if (position <= no_elements/2){
+        current_positon = 0;
+        iterator = head;
+        while (current_positon < position){
+            iterator = iterator->next;
+            current_positon++;
+        }
+        previous_element = iterator;
+        next_element = iterator->next;
+    }else{
+        current_positon = no_elements;
+        iterator = tail;
+        while (current_positon > position){
+            iterator = iterator->previous;
+            current_positon--;
+        }
+        previous_element = iterator->previous;
+        next_element = iterator;
+    }
+
+    new_element = malloc(sizeof(struct g_node));
+    new_element->info = new_element_value;
+    new_element->next = next_element;
+    new_element->previous = previous_element;
+    next_element->previous = new_element;
+    previous_element->next = new_element;
+    head->info++;
+}
+
+void print_list(struct g_node *head, struct g_node *tail){
+    struct g_node *iterator = head;
+
+    printf("\n List: ");
+    while (iterator->next != tail) {
+        printf("%d ", iterator->next->info);
+        iterator = iterator->next;
+    }
+}
+
+///to be implemented
+void print_list_reverse(struct g_node *head, struct g_node *tail){
+    struct g_node *iterator;
+
+}
+
+int pop_element_begining(struct g_node *head, struct g_node *tail){
+    struct g_node *poped_element;
+    struct g_node *next_element;
+    int aux = 0;
+
+    assert(head->next != tail);
+
+    poped_element = head->next;
+    next_element = head->next->next;
+    head->next = next_element;
+    next_element->previous = head;
+    aux = poped_element->info;
+    free(poped_element);
+    head->info--;
+
+    return aux;
+}
+
+///to be implemented
+int pop_element_end(struct g_node *head, struct g_node *tail){
+    struct g_node *poped_element;
+    struct g_node *previous_element;
+    int aux = 0;
+
+    return aux;
+}
+
+///to be implemented
+int pop_element_at_position(struct g_node *head, struct g_node *tail, int position){
+    int no_elements;
+    int current_positon;
+    int aux = 0;
+    struct g_node *iterator;
+    struct g_node *previous_element;
+    struct g_node *next_element;
+    struct g_node *poped_element;
+
+
+    return aux;
+}
+
+void empty_list(struct g_node *head, struct g_node *tail){
+    struct g_node *iterator_left;
+    struct g_node *iterator_right;
+
+    assert(head->next != tail);
+    iterator_left = head->next;
+    iterator_right = tail->previous;
+
+    while( 1 > 0 ){
+        if (iterator_left == iterator_right){
+            free(iterator_left);
+            break;
+        }
+        if (iterator_left->next == iterator_right){
+            free(iterator_left);
+            free(iterator_right);
+            break;
+        }
+        iterator_left = iterator_left->next;
+        free(iterator_left->previous);
+        iterator_right = iterator_right->previous;
+        free(iterator_right->next);
+    }
+
+    head->next = tail;
+    head->info = 0;
+    tail->previous = head;
+}
+
+//to be implemented
+void selection_sort(struct g_node *head){
+    struct g_node *min;
+    struct g_node *iterator_1;
+    struct g_node *iterator_2;
+
+}
+
+int main(){
+    struct g_node *head = malloc(sizeof(struct g_node));
+    struct g_node *tail = malloc(sizeof(struct g_node));
+    int aux;
+    int aux2;
+
+    head->previous = NULL;
+    head->next = tail;
+    head->info = 0;
+    tail->previous = head;
+    tail->next = NULL;
+
+    printf("\n===== Pushing at the begining ======");
+    push_element_begining(head, tail, 1);
+    print_list(head,tail);
+    push_element_begining(head, tail, 2);
+    print_list(head,tail);
+    push_element_begining(head, tail, 3);
+    print_list(head, tail);
+
+    printf("\n===== Printing reverse ======");
+    print_list_reverse(head, tail);
+
+    printf("\n===== Pushing at the end ======");
+    print_list(head, tail);
+    push_element_end(head, tail, 4);
+    print_list(head, tail);
+    push_element_end(head, tail, 5);
+    print_list(head, tail);
+    push_element_end(head, tail, 6);
+    print_list(head, tail);
+
+    printf("\n===== No elements in list ======");
+    aux = get_no_elements(head, tail);
+    printf("\nThe list has %d elements ", aux);
+    printf("\nThe list has %d elements ", head->info);
+
+    printf("\n===== Pushing at specific position ======");
+    print_list(head, tail);
+    push_element_at_position(head,tail,2,10);
+    print_list(head, tail);
+
+    printf("\n===== Poping from the begining ======");
+    print_list(head, tail);
+    aux = pop_element_begining(head, tail);
+    printf("\n The poped element is %d",aux);
+    print_list(head, tail);
+
+    printf("\n===== Poping from the end ======");
+    print_list(head, tail);
+    aux = pop_element_end(head, tail);
+    printf("\n The poped element is %d",aux);
+    print_list(head, tail);
+
+    printf("\n===== Poping from specific position ======");
+    print_list(head, tail);
+    aux = pop_element_at_position(head, tail, 4);
+    printf("\n The poped element is %d",aux);
+    print_list(head, tail);
+
+    printf("\n===== No elements in list ======");
+    aux = get_no_elements(head, tail);
+    printf("\nThe list has %d elements ", aux);
+    printf("\nThe list has %d elements ", head->info);
+
+    printf("\n===== Empty list ======");
+    print_list(head, tail);
+    selection_sort(head);
+    print_list(head, tail);
+
+    printf("\n===== Selection Sort ======");
+    print_list(head, tail);
+    empty_list(head, tail);
+    print_list(head, tail);
+
+    printf("\n===== Queue (FIFO) ======");
+    push_element_begining(head, tail, 1);
+    push_element_begining(head, tail, 2);
+    push_element_begining(head, tail, 3);
+    print_list(head, tail);
+    pop_element_end(head, tail);
+    print_list(head,tail);
+    empty_list(head, tail);
+
+    printf("\n===== Stack (LIFO) ======");
+    push_element_begining(head, tail, 1);
+    push_element_begining(head, tail, 2);
+    push_element_begining(head, tail, 3);
+    print_list(head, tail);
+    pop_element_begining(head, tail);
+    print_list(head,tail);
+    empty_list(head, tail);
+
+    free(head);
+    free(tail);
+
+    return 0;
+}
---
+++ Linked Lists/Simple Linked Lists/main.c
@@ -0,0 +1,219 @@
+//
+// Created by Alexandru Becheru on 20/03/2018.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+struct g_node{
+    int info;
+    struct g_node *next;
+};
+
+void push_element_begining(struct g_node *head ,int new_element_value){
+    struct g_node *new_element = malloc(sizeof(struct g_node));
+
+    new_element->info = new_element_value;
+    new_element->next = head->next;
+    head->next = new_element;
+}
+
+void push_element_end(struct g_node *head ,int new_element_value){
+    struct g_node *new_element = malloc(sizeof(struct g_node));
+    struct g_node *iterator = head;
+    struct g_node *last_element;
+
+    while (iterator->next != NULL) {
+        iterator = iterator->next;
+    }
+    last_element = iterator;
+
+    last_element->next = new_element;
+    new_element->info = new_element_value;
+    new_element->next = NULL;
+}
+
+///to be implemented
+void push_element_at_position(struct g_node *head, int position, int new_element_value){
+    struct g_node *iterator;
+    struct g_node *previous_element;
+    struct g_node *new_element;
+    int current_position;
+
+    assert(position >= 0);
+    new_element = malloc(sizeof(struct g_node));
+    new_element->info = new_element_value;
+    iterator = head;
+    current_position = 0;
+
+    while( (current_position < position) && (iterator->next != NULL)){
+        current_position++;
+        iterator = iterator->next;
+    }
+
+    previous_element = iterator;
+    new_element->next = previous_element->next;
+    previous_element->next = new_element;
+}
+
+void print_list(struct g_node *head){
+    struct g_node *iterator = head;
+
+    printf("\n List: ");
+    while (iterator->next != NULL) {
+        printf("%d ", iterator->next->info);
+        iterator = iterator->next;
+    }
+}
+
+///to be implemented
+int find_postion_of_value(struct g_node *head ,int element_value){
+    int position = -1; //list is empty
+    struct g_node *iterator = head;
+    int element_found = 0;
+
+    while (iterator->next != NULL) {
+        position++;
+        if (iterator->next->info == element_value){
+            element_found = 1;
+            break;
+        }
+        iterator = iterator->next;
+    }
+
+    if (element_found == 1){
+        return position;
+    }else{
+        return -2; // not found in list
+    }
+
+}
+
+///to be implemented
+int pop_element_begining(struct g_node *head){
+    struct g_node *poped_element;
+    int aux;
+
+    poped_element = head->next;
+    head->next = poped_element->next;
+    aux = poped_element->info;
+    free(poped_element);
+
+    return aux;
+}
+
+///to be implemented
+int pop_element_end(struct g_node *head){
+    struct g_node *poped_element;
+    struct g_node *iterator;
+    int aux;
+
+    iterator = head;
+    while (iterator->next->next != NULL) {
+        iterator = iterator->next;
+    }
+
+    poped_element = iterator->next;
+    iterator->next = poped_element->next;
+    aux = poped_element->info;
+    free(poped_element);
+
+    return aux;
+}
+
+///to be implemented
+int pop_element_at_position(struct g_node *head, int position){
+    struct g_node *iterator;
+    struct g_node *poped_element;
+    struct g_node *previous_element;
+    int current_position;
+    int aux;
+
+    assert(position >= 0);
+    iterator = head;
+    current_position = 0;
+
+    while( (current_position < position) && (iterator->next != NULL)){
+        current_position++;
+        iterator = iterator->next;
+    }
+
+    previous_element = iterator;
+    poped_element = previous_element->next;
+    previous_element->next = poped_element->next;
+    aux = poped_element->info;
+    free(poped_element);
+
+    return aux;
+}
+
+///to be implemented
+int return_no_elements(struct g_node *head){
+    int no_elements ;
+    struct g_node *iterator;
+    iterator = head;
+    no_elements = 0;
+
+    while (iterator->next != NULL){
+        no_elements++;
+        iterator = iterator->next;
+    }
+
+    return no_elements;
+}
+
+int main(){
+    struct g_node *head = malloc(sizeof(struct g_node));
+    int aux;
+    int aux2;
+
+    head->next = NULL;
+
+    printf("\n===== Pushing at the begining ======");
+    push_element_begining(head, 1);
+    print_list(head);
+    push_element_begining(head, 2);
+    print_list(head);
+
+    printf("\n===== Pushing at the end ======");
+    push_element_end(head, 3);
+    print_list(head);
+
+    printf("\n===== Pushing at specific position ======");
+    print_list(head);
+    push_element_at_position(head,3,10);
+    print_list(head);
+
+    printf("\n===== No elements in list ======");
+    aux = return_no_elements(head);
+    printf("\nThe list has %d elments ", aux);
+
+    printf("\n===== Finding the position of a specific element ======");
+    printf("\nGive the searched element's value: ");
+    scanf("%d",&aux);
+    aux2 = find_postion_of_value(head,aux);
+    printf("\nThe position of element %d is %d", aux, aux2);
+
+    printf("\n===== Poping from the begining ======");
+    aux = pop_element_begining(head);
+    printf("\n The poped element is %d",aux);
+    print_list(head);
+
+    printf("\n===== Poping from the end ======");
+    aux = pop_element_end(head);
+    printf("\n The poped element is %d",aux);
+    print_list(head);
+
+    printf("\n===== Poping from specific position ======");
+    push_element_begining(head,2);
+    push_element_begining(head,3);
+    print_list(head);
+    aux = pop_element_at_position(head,0);
+    printf("\n The poped element is %d",aux);
+    print_list(head);
+
+    free(head);
+
+    return 0;
+}
